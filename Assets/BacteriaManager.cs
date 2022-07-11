469b988c7201cc1e1517ea1182c23c31a50f8f26
ollulf <max.bredlau@googlemail.com>
2022-07-11 16:11:44 +0200

    Added UI for Bacteria minigame

--- Assets/BacteriaManager.cs
+++ Assets/BacteriaManager.cs
@@ -2,16 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BacteriaManager : MonoBehaviour
 {
     [SerializeField] GameObject bacteriaObject;
     [SerializeField] Transform spawnPoint;
 
+    [SerializeField] TextMeshProUGUI timerUI, scoreUI, buttonUI, highscoreUI;
+
     [SerializeField] private KeyCode[] keyCodes;
     private KeyCode currendKey;
 
     private int score = 0;
+    private int highscore = 0;
+    private int time = 30;
+    private Timer timer;
 
     // Start is called before the first frame update
     void Start()
@@ -20,23 +26,49 @@ public class BacteriaManager : MonoBehaviour
         GameObject obj = Instantiate(bacteriaObject,spawnPoint);
         obj.GetComponent<Bacteria>().SetCenterTransform(spawnPoint);
 
-        Debug.Log("Press " + currendKey);
+        //Debug.Log("Press " + currendKey);
+
+        timer = new Timer();
+        timer.SetStartTime(time);
     }
 
   
     // Update is called once per frame
     void Update()
     {
+        timer.Tick();
+
         if(Input.GetKeyDown(currendKey))
         {
             score++;
             SetNewKey();
-            Debug.Log("Pressed Correct Key!");
-            Debug.Log("Press " + currendKey);
-            GameObject obj = Instantiate(bacteriaObject, spawnPoint);
+            GameObject obj = Instantiate(bacteriaObject,spawnPoint);
             obj.GetComponent<Bacteria>().SetCenterTransform(spawnPoint);
+            obj.GetComponent<Rigidbody>().AddForce(UnityEngine.Random.Range(0, 5), UnityEngine.Random.Range(0,5), UnityEngine.Random.Range(0, 5));
 
-        } 
+        }
+        UpdateUI();
+        CheckTimer();
+        
+    }
+
+    private void CheckTimer()
+    {
+        if(timer.CurrentTime < 0)
+        {
+            timer.ResetTimer();
+            highscore = score;
+            score = 0;
+            SetNewKey();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        timerUI.SetText(""+timer.CurrentTime.ToString("0"));
+        scoreUI.SetText("" + score);
+        buttonUI.SetText("" + currendKey);
+        highscoreUI.SetText("" + highscore);
     }
 
     private void SetNewKey()

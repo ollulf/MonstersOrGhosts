eb12d1cb64cd3cbc77ad86bf7c8c029b2d4b68bc
ollulf <max.bredlau@googlemail.com>
2022-08-12 17:53:17 +0200

    Added Sounds for Camera

--- Assets/98_Scripts/Camera/CameraScroller.cs
+++ Assets/98_Scripts/Camera/CameraScroller.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using NaughtyAttributes;
+using System;
 
 public class CameraScroller : MonoBehaviourPun
 {
     [SerializeField] Transform farPosition, closePosition, animalSight;
     [SerializeField] AnimationCurve acellerationCurve;
     [SerializeField] float speed = 1f;
+
+    [SerializeField] private AudioSource underwaterAmbient, enterWaterSound, exitWaterSound;
+    [ShowNonSerializedField]private bool isCameraUnderwater;
 
     [ShowNonSerializedField] private float distance, currentDistance = 1;
 
@@ -29,6 +33,10 @@ public class CameraScroller : MonoBehaviourPun
     {
         gameObject.transform.position = closePosition.position;
         distance = Vector3.Distance(farPosition.position, closePosition.position);
+
+        underwaterAmbient.Play();
+        //if (!(gameObject.transform.position.y < 12))
+            //underwaterAmbient.Pause();
     }
 
     public void SetFarPosition(Transform newTransform)
@@ -44,6 +52,8 @@ public class CameraScroller : MonoBehaviourPun
     // Update is called once per frame
     void Update()
     {
+        UpdateAmbientSound();
+
         distance = Vector3.Distance(farPosition.position, closePosition.position);
         float t = acellerationCurve.Evaluate(currentDistance);
 
@@ -66,8 +76,32 @@ public class CameraScroller : MonoBehaviourPun
         }
 
 
-        
+
         IndexChecker();
+    }
+
+    private void UpdateAmbientSound()
+    {
+        bool tempUnderWater = isCameraUnderwater;
+        isCameraUnderwater = (gameObject.transform.position.y < 12);
+
+        if (isCameraUnderwater != tempUnderWater)
+        {
+            if (isCameraUnderwater)
+            {
+                Debug.Log("Enters Water");
+                enterWaterSound.Play();
+                underwaterAmbient.UnPause();
+
+            }
+            if (!isCameraUnderwater)
+            {
+                Debug.Log("Exits Water");
+                exitWaterSound.Play();
+                underwaterAmbient.Pause();
+            }
+        }
+
     }
 
     private void IndexChecker()
@@ -78,9 +112,9 @@ public class CameraScroller : MonoBehaviourPun
                 {
                     gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, animalSight.position, speed * Time.deltaTime);
                     Vector3 rotation = new Vector3(
-    Mathf.LerpAngle(gameObject.transform.eulerAngles.x, animalSight.eulerAngles.x, speed * Time.deltaTime),
-    Mathf.LerpAngle(gameObject.transform.eulerAngles.y, animalSight.eulerAngles.y, speed * Time.deltaTime),
-    Mathf.LerpAngle(gameObject.transform.eulerAngles.z, animalSight.eulerAngles.z, speed * Time.deltaTime));
+                            Mathf.LerpAngle(gameObject.transform.eulerAngles.x, animalSight.eulerAngles.x, speed * Time.deltaTime),
+                            Mathf.LerpAngle(gameObject.transform.eulerAngles.y, animalSight.eulerAngles.y, speed * Time.deltaTime),
+                            Mathf.LerpAngle(gameObject.transform.eulerAngles.z, animalSight.eulerAngles.z, speed * Time.deltaTime));
                     gameObject.transform.eulerAngles = rotation;
                     break;
                 }
@@ -88,9 +122,9 @@ public class CameraScroller : MonoBehaviourPun
                 {
                     gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, closePosition.position, speed * Time.deltaTime);
                     Vector3 rotation = new Vector3(
-Mathf.LerpAngle(gameObject.transform.eulerAngles.x, closePosition.eulerAngles.x, speed * Time.deltaTime),
-Mathf.LerpAngle(gameObject.transform.eulerAngles.y, closePosition.eulerAngles.y, speed * Time.deltaTime),
-Mathf.LerpAngle(gameObject.transform.eulerAngles.z, closePosition.eulerAngles.z, speed * Time.deltaTime));
+                            Mathf.LerpAngle(gameObject.transform.eulerAngles.x, closePosition.eulerAngles.x, speed * Time.deltaTime),
+                            Mathf.LerpAngle(gameObject.transform.eulerAngles.y, closePosition.eulerAngles.y, speed * Time.deltaTime),
+                            Mathf.LerpAngle(gameObject.transform.eulerAngles.z, closePosition.eulerAngles.z, speed * Time.deltaTime));
                     gameObject.transform.eulerAngles = rotation;
                     fin = false;
                     break;

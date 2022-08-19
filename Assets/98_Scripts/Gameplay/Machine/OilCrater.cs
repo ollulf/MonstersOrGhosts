b988ec5fe0873f127e7c59b047bdfa6931ec1b7e
Meltdragon <dragonheart84261@aol.com>
2022-08-19 12:23:39 +0200

    Choice Prefab

--- Assets/98_Scripts/Gameplay/Machine/OilCrater.cs
+++ Assets/98_Scripts/Gameplay/Machine/OilCrater.cs
@@ -5,12 +5,23 @@ using Photon.Pun;
 
 public class OilCrater : MonoBehaviourPun
 {
-    public void Down()
+    public void BuildOilRig()
     {
         if ((Charakter)PhotonNetwork.LocalPlayer.CustomProperties["PlayerCharakter"] == Charakter.Machine && RefineryHandler.Money >= RefineryHandler.RefineryCost)
         {
             base.photonView.RequestOwnership();
-            PhotonNetwork.Instantiate("MachineGame/seaRefinery", new Vector3(transform.position.x, 13.6f, transform.position.z), Quaternion.identity);
+            PhotonNetwork.Instantiate("MachineGame/seaRefinery", new Vector3(transform.parent.root.position.x , 13.6f, transform.parent.root.position.z), Quaternion.identity);
+            RefineryHandler.SetMoney();
+            PhotonNetwork.Destroy(photonView.gameObject);
+        }
+    }
+
+    public void BuildEnvoirment()
+    {
+        if ((Charakter)PhotonNetwork.LocalPlayer.CustomProperties["PlayerCharakter"] == Charakter.Machine && RefineryHandler.Money >= RefineryHandler.RefineryCost)
+        {
+            base.photonView.RequestOwnership();
+            PhotonNetwork.Instantiate("MachineGame/military-headquarters", new Vector3(transform.parent.root.position.x, 13.6f, transform.parent.root.position.z), Quaternion.identity);
             RefineryHandler.SetMoney();
             PhotonNetwork.Destroy(photonView.gameObject);
         }

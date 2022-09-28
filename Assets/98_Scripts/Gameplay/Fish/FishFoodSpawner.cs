558b7498c1030d7981c2a47128f053b0563db2ef
Meltdragon <dragonheart84261@aol.com>
2022-09-28 15:16:09 +0200

    FishFoodSpawner Label

--- Assets/98_Scripts/Gameplay/Fish/FishFoodSpawner.cs
+++ Assets/98_Scripts/Gameplay/Fish/FishFoodSpawner.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEditor;
+using NaughtyAttributes;
 
 public class FishFoodSpawner : MonoBehaviourPun
 {
@@ -43,4 +45,35 @@ public class FishFoodSpawner : MonoBehaviourPun
             }
         }
     }
+#if UNITY_EDITOR
+
+    [Button]
+    private void CreateFishFoodPlace()
+    {
+        GameObject place = new GameObject("FoodSpawnPlace");
+        place.transform.parent = this.gameObject.transform;
+
+        DrawIcon(place, 4);
+        UnityEditorInternal.ComponentUtility.CopyComponent(this);
+        UnityEditorInternal.ComponentUtility.PasteComponentValues(this);
+    }
+
+
+    private void DrawIcon(GameObject gameObject,int idx)
+    {
+        GUIContent[] icons = GetTextures("sv_label_", string.Empty, 0, 8);
+        GUIContent icon = icons[idx];
+        EditorGUIUtility.SetIconForObject(gameObject, (Texture2D)icon.image);
+    }
+
+    private GUIContent[] GetTextures(string baseName, string postFix, int startIndex, int count)
+    {
+        GUIContent[] Iconarray = new GUIContent[count];
+        for (int i = 0; i < count; i++)
+        {
+            Iconarray[i] = EditorGUIUtility.IconContent(baseName + (startIndex + i) + postFix);
+        }
+        return Iconarray;
+    }
+#endif
 }

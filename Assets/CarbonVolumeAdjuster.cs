6df78fba34af274476712f2e8ff02b048936b2c4
ollulf <max.bredlau@googlemail.com>
2022-10-19 14:07:08 +0200

    Added Carbon Dioxite Perception and Deer Collider

---
+++ Assets/CarbonVolumeAdjuster.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class CarbonVolumeAdjuster : MonoBehaviour
+{
+    [SerializeField] public VolumeProfile volumeProfile;
+    private ColorAdjustments colorAdjustments;
+
+    public float tempIncreaseUntilStart = 0, tempIncreaseMax = 5;
+    public float startContrast = 0, endContrast = -20;
+
+    public float curentTempIncrease;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ColorAdjustments temp;
+        if (volumeProfile.TryGet<ColorAdjustments>(out temp))
+        {
+            colorAdjustments = temp;
+            colorAdjustments.contrast.overrideState = true;
+        }
+    }
+    void Update()
+    {
+        UpdateVolumeConstrast();
+    }
+
+    private void UpdateVolumeConstrast()
+    {
+        Mathf.Clamp(colorAdjustments.contrast.value = startContrast - (curentTempIncrease / (tempIncreaseMax - tempIncreaseUntilStart) * (Mathf.Abs(startContrast - endContrast))), startContrast, endContrast);
+    }
+}

a98c94656d2a0f66034d4e4c515c8d4a349a24e1
ollulf <max.bredlau@googlemail.com>
2022-11-04 15:49:16 +0100

    Worked on TempShader

---
+++ Assets/TextureSwitcher.cs
@@ -0,0 +1,50 @@
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureSwitcher : MonoBehaviour
+{
+    [SerializeField] MeshRenderer meshRenderer;
+
+    [SerializeField] Texture2D[] textures;
+    [SerializeField, ReadOnly] Material materialInstance;
+    [ShowNonSerializedField] float currentSin;
+
+    public float sinMin = -1, sinMax = 1;
+    public int nextIndex = 2;
+    private bool goesUp = true;
+    // Start is called before the first frame update
+
+    private void Awake() => materialInstance = meshRenderer.material;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSin = Time.time % 4 / 4;
+        if (!goesUp) currentSin = 1 - currentSin;
+
+        materialInstance.SetFloat("_LerpValue", currentSin);
+
+        if (currentSin <= sinMin && !goesUp)
+        {
+            Debug.LogWarning("Reached sin MIN");
+            materialInstance.SetTexture("_Texture1", textures[nextIndex]);
+            nextIndex++;
+            goesUp = true;
+        }
+        else if (currentSin >= sinMax && goesUp)
+        {
+            Debug.LogWarning("Reached sin MAX");
+            materialInstance.SetTexture("_Texture2", textures[nextIndex]);
+            nextIndex++;
+            goesUp = false;
+        }
+
+        if(nextIndex == textures.Length) nextIndex = textures.Length - 1;
+    }
+}
